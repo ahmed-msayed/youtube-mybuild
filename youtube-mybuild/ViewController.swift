ae15d974f386c311c035fe5a63ff96b4480fe760
ahmed-msayed <gta.ahmed@gmail.com>
2020-10-25 13:44:13 +0200

    test bitrise 2

--- youtube-mybuild/ViewController.swift
+++ youtube-mybuild/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         var i = 0
+        var x = 0
     }
 
 
